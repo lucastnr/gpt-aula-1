e8bcfd6ed42a9df2be9e6e6b1132b0c68a6ddd35
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-12 22:23:06 -0300

    Add system message

--- src/gpt.js
+++ src/gpt.js
@@ -4,19 +4,29 @@ const url = "https://api.openai.com/v1/chat/completions";
 
 export const chat = async (content) => {
   // Mandar pra OpenAI
-  const response = await axios.post(url, {
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "user",
-        content
-      }
-    ]
-  }, {
-    headers: {
-      "Authorization": "Bearer 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"
+  const response = await axios.post(
+    url,
+    {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "Seu nome é Lucas Tenório e você é um programador. Seu objetivo é tirar dúvida de programação apenas, você não pode falar sobre qualquer outra assunto que não seja programação",
+        },
+        {
+          role: "user",
+          content,
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization:
+          "Bearer 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",
+      },
     }
-  });
+  );
 
   return response;
 };
