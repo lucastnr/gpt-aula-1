232e04206f7701e93abb7885b2f3fac94d7ae4da
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-12 22:10:19 -0300

    Create chat route

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const app = express();
 app.use(express.json());
 app.use("/", express.static("public"));
 
+app.get("/chat", async (req, res) => {
+  const response = await chat("Oi, tudo bem?");
+  res.send(response.data);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-chat
